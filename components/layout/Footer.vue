df910a17bc2c059f41f5d970c5c508c613005679
gurbanli97 <ali.e.gurbanli@gmail.com>
2022-06-06 19:01:12 +0400

    fix

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 <footer class="text-center text-lg-start bg-light text-muted footer">
   <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-    © 2021 Copyright - 
+    © 2022 Copyright - 
     <a class="text-reset fw-bold" href="https://github.com/gurbanli97" target="_blank">
     <strong>Ali Gurbanli</strong>
     </a>
